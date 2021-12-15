6678298cdcf578a77d75ed6300fb3b8c6eebdca6
Arisa Okasaka <68284764+arisaokasaka@users.noreply.github.com>
2021-12-15 23:14:24 +0900

    how to handle cookies

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 const express = require('express')
 const app = express()
 const ejs = require('ejs')
-const bodyParser = require('body-parser') // body-parserの初期化
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const port = 3000
 
 app.engine('ejs', ejs.renderFile) // ejs使うよーっていう宣言
-app.use(bodyParser.urlencoded({
+app.use(bodyParser.urlencoded({ // body-parserの初期化
   extended: true
 }))
+app.use(cookieParser())
+// cookie-parserの利用宣言。
+// cookie-parserをつかと、リクエストオブジェクトのcookiesプロパティにcookieの値がまとめられる
 
 app.get('/', (req, res) => {
   console.log('---GET Request---')
   console.log('nameは' + req.query.name) // get通信はquery
   console.log('ageは' + req.query.age)
+
+  let count = req.cookies.count == undefined ? 0 : req.cookies.count
+  count++
+  res.cookie('count', count, {maxAge: 5000})
+
   res.render('temp.ejs', {
-    contents: '<p>content sentence!</p>'
+    contents: '<p>content sentence!</p>',
+    count: count
   })
 })
 
