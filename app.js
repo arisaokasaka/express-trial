79fb99cb58855986b8741a36112841ac8a849602
Arisa Okasaka <68284764+arisaokasaka@users.noreply.github.com>
2021-12-15 23:26:35 +0900

    how to handle session

--- app.js
+++ app.js
@@ -3,28 +3,45 @@ const app = express()
 const ejs = require('ejs')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
+const session = require('express-session')
 const port = 3000
 
-app.engine('ejs', ejs.renderFile) // ejs使うよーっていう宣言
-app.use(bodyParser.urlencoded({ // body-parserの初期化
+// ejs使うよーっていう宣言
+app.engine('ejs', ejs.renderFile)
+
+// body-parserの初期化
+app.use(bodyParser.urlencoded({
   extended: true
 }))
-app.use(cookieParser())
+
 // cookie-parserの利用宣言。
-// cookie-parserをつかと、リクエストオブジェクトのcookiesプロパティにcookieの値がまとめられる
+// cookie-parserを使うと、リクエストオブジェクトのcookiesプロパティにcookieの値がまとめられる
+app.use(cookieParser())
+
+// session-parserを使うと、リクエストオブジェクトのsessionsプロパティにsessionの値がまとめられる
+app.use(session({
+  secret: 'session-secret', // セッションのハッシュ化に使用する任意の文字列
+  resave: true, // リクエストのたびに変更がなくてもセッションデータを保存し直すかどうか
+  saveUninitialized: true, // 未初期化状態のセッションを保存するかどうか
+}))
 
 app.get('/', (req, res) => {
   console.log('---GET Request---')
   console.log('nameは' + req.query.name) // get通信はquery
   console.log('ageは' + req.query.age)
 
-  let count = req.cookies.count == undefined ? 0 : req.cookies.count
-  count++
-  res.cookie('count', count, {maxAge: 5000})
+  let cookieCount = req.cookies.cookieCount == undefined ? 0 : req.cookies.cookieCount
+  cookieCount++
+  res.cookie('cookieCount', cookieCount, {maxAge: 5000})
+
+  let sessionCount = req.session.sessionCount == undefined ? 0 : req.session.sessionCount
+  sessionCount++
+  req.session.sessionCount = sessionCount
 
   res.render('temp.ejs', {
     contents: '<p>content sentence!</p>',
-    count: count
+    cookieCount: cookieCount,
+    sessionCount: sessionCount,
   })
 })
 
