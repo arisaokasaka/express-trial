e84955f98daa98f75779477eb7e6cc9d8a99eded
Arisa Okasaka <68284764+arisaokasaka@users.noreply.github.com>
2021-12-15 23:07:12 +0900

    add body parser and get post transmission

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const express = require('express')
 const app = express()
+const ejs = require('ejs')
+const bodyParser = require('body-parser') // body-parserの初期化
 const port = 3000
 
-app.get('/', (_req, res) => {
-  res.send('Hello World!')
+app.engine('ejs', ejs.renderFile) // ejs使うよーっていう宣言
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+app.get('/', (req, res) => {
+  console.log('---GET Request---')
+  console.log('nameは' + req.query.name) // get通信はquery
+  console.log('ageは' + req.query.age)
+  res.render('temp.ejs', {
+    contents: '<p>content sentence!</p>'
+  })
+})
+
+app.post('/', (req, res) => {
+  console.log('---POST Request---')
+  console.log('nameは' + req.body.name) // post通信はbody
+  console.log('ageは' + req.body.age)
+  res.render('temp.ejs', {
+    contents: '<p>content sentence!</p>'
+  })
 })
 
 app.listen(port, () => {
